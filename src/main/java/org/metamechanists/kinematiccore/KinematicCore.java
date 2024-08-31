1931fe93d642980b73784c98806e5380d2ea7863
LordIdra <percy.baker.0@gmail.com>
2024-08-31 18:37:17 +0100

    Start testing MapDB

--- src/main/java/org/metamechanists/kinematiccore/KinematicCore.java
+++ src/main/java/org/metamechanists/kinematiccore/KinematicCore.java
@@ -1,11 +1,38 @@
 package org.metamechanists.kinematiccore;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+import org.metamechanists.kinematiccore.api.KinematicEntity;
+import org.metamechanists.kinematiccore.api.TestEntity;
+
+import java.io.File;
+import java.util.UUID;
 
 
 public class KinematicCore extends JavaPlugin {
+    private DB db;
+    private HTreeMap<UUID, KinematicEntity> entities;
+
     @Override
     public void onEnable() {
+        db = DBMaker.fileDB(new File(getDataFolder(), "data.mapdb"))
+                .closeOnJvmShutdown()
+                .transactionEnable()
+                .make();
 
+        entities = db.hashMap("entities")
+                .keySerializer(Serializer.UUID)
+                .valueSerializer(Serializer.JAVA)
+                .createOrOpen();
+
+        entities.put(new UUID(1, 2), new TestEntity());
+    }
+
+    @Override
+    public void onDisable() {
+        db.close();
     }
 }
---
+++ src/main/java/org/metamechanists/kinematiccore/api/TestEntity.java
@@ -0,0 +1,5 @@
+package org.metamechanists.kinematiccore.api;
+
+public class TestEntity extends KinematicEntity {
+    private int bruh = 2;
+}
