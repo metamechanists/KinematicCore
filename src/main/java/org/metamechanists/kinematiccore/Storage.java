1be840708890125c5b6c5e95eb622ab690266691
LordIdra <percy.baker.0@gmail.com>
2024-08-31 17:18:34 +0100

    bruh

---
+++ src/main/java/org/metamechanists/kinematiccore/Storage.java
@@ -0,0 +1,4 @@
+package org.metamechanists.kinematiccore;
+
+public class Storage {
+}
---
+++ src/main/java/org/metamechanists/kinematiccore/api/KinematicEntity.java
@@ -0,0 +1,6 @@
+package org.metamechanists.kinematiccore.api;
+
+
+public abstract class KinematicEntity {
+
+}
