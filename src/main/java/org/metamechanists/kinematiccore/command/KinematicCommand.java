c56500fd621b7e088298916caa1acb620fa6f498
LordIdra <percy.baker.0@gmail.com>
2024-09-07 14:29:51 +0100

    tests

--- src/main/java/org/metamechanists/kinematiccore/command/KinematicCommand.java
+++ src/main/java/org/metamechanists/kinematiccore/command/KinematicCommand.java
@@ -22,21 +22,25 @@ public class KinematicCommand extends BaseCommand {
     private static final TextColor HEADING_COLOR = TextColor.color(100, 255, 100);
 
     @HelpCommand
+    @Description("Show this help screen")
     public static void help(@NotNull CommandSender sender, @NotNull CommandHelp help) {
         sender.sendMessage(Component.text("[[ Kinematic Core ]]").color(HEADING_COLOR).decorate(TextDecoration.BOLD));
         help.showHelp();
     }
 
+    @SuppressWarnings("InnerClassMayBeStatic")
     @Subcommand("dev")
-    @Description("Commands to help with Kinematic development. DO NOT use this command outside of a test server unless you are sure you know what you're doing.")
-    public static class KinematicDevCommand extends BaseCommand {
+    @Description("Commands to help with Kinematic development. DO NOT use this command outside of a test server unless you know exactly what you're doing!")
+    public class KinematicDevCommand extends BaseCommand {
 
+        @SuppressWarnings("MethodMayBeStatic")
         @Subcommand("test")
         @Description("Run Kinematic tests.")
         public static class KinematicTestCommand extends BaseCommand {
+
             @Subcommand("all")
             @Description("Run all 'non-destructive' tests.")
-            public static void test(@NotNull Player player) {
+            public void test(@NotNull Player player) {
                 MainTester.TestResult result = new MainTester(player.getLocation()).allNonDestructive();
 
                 player.sendMessage(ChatColor.GRAY + "[ "
