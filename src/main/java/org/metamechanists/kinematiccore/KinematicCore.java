51f2cfe32da1d2f238059d3771c5731a96e8dd51
LordIdra <percy.baker.0@gmail.com>
2024-09-07 23:47:58 +0100

    tests

--- src/main/java/org/metamechanists/kinematiccore/KinematicCore.java
+++ src/main/java/org/metamechanists/kinematiccore/KinematicCore.java
@@ -2,11 +2,9 @@ package org.metamechanists.kinematiccore;
 
 import co.aikar.commands.PaperCommandManager;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.metamechanists.kinematiccore.api.storage.EntityStorage;
 import org.metamechanists.kinematiccore.api.KinematicAddon;
-import org.metamechanists.kinematiccore.api.TestListener;
 import org.metamechanists.kinematiccore.api.entity.KinematicEntityTicker;
 import org.metamechanists.kinematiccore.command.KinematicCommand;
 
@@ -20,7 +18,6 @@ public class KinematicCore extends JavaPlugin implements KinematicAddon {
         instance = this;
         EntityStorage.init();
         KinematicEntityTicker.init();
-        Bukkit.getServer().getPluginManager().registerEvents(new TestListener(), this);
         PaperCommandManager manager = new PaperCommandManager(this);
         manager.enableUnstableAPI("help");
         manager.registerCommand(new KinematicCommand());
