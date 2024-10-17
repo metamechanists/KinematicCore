f4f3fd79eb04ecafc7838b25f4f10d1a4eb95561
LordIdra <percy.baker.0@gmail.com>
2024-10-17 19:02:25 +0100

    okay really?

--- src/main/java/org/metamechanists/kinematiccore/api/addon/KinematicAddon.java
+++ src/main/java/org/metamechanists/kinematiccore/api/addon/KinematicAddon.java
@@ -1,5 +1,16 @@
 package org.metamechanists.kinematiccore.api.addon;
 
+import org.metamechanists.kinematiccore.KinematicCore;
+
+
+@SuppressWarnings({"InterfaceMayBeAnnotatedFunctional", "unused"})
 public interface KinematicAddon {
     String name();
+    default void cleanup() {
+        try {
+            KinematicCore.cleanup(this);
+        } catch (NoClassDefFoundError e) {
+            // Kinematic already disabled so should have saved addon data already
+        }
+    }
 }
